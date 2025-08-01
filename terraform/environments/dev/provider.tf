996ff9e8f81ed148492a1bedebf8b932399352f8
dansarpong <dansarpong.me@gmail.com>
2025-08-01 09:04:40 +0000

    feat: add backend configuration for S3 in provider.tf

--- terraform/environments/dev/provider.tf
+++ terraform/environments/dev/provider.tf
@@ -1,8 +1,18 @@
 provider "aws" {
-  region = "us-east-1"
+  region  = "us-east-1"
   profile = var.aws_profile
 
   default_tags {
     tags = var.tags
+  }
+}
+
+terraform {
+  backend "s3" {
+    bucket       = "dansarpong-tf"
+    key          = "mlops-capstone/terraform/environments/dev/terraform.tfstate"
+    region       = "us-east-1"
+    encrypt      = true
+    use_lockfile = true
   }
 }
